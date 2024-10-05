1263655bc7008d040b914ccceb73a2775bf39d2d
Emiliano Portillo <EmilianoMt>
2024-10-04 22:14:33 -0600

    Swagger Api Bearer and Tags

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   .setTitle('Ocso API')
   .setDescription('API for Oxxo management') 
   .setVersion('0.9')
+  .addBearerAuth()
   .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
