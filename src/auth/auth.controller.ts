16c1aa11da9d78a3d1d1c7237e718c1049c40b7c
Emiliano Portillo <EmilianoMt>
2024-10-18 14:53:45 -0600

    Feat: Cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Post, Body, Patch, Param, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { LoginUserDto } from './dto/login-user.dto';
+import {TOKEN_NAME} from '../auth/constants/jwt.constants'
+import { Response } from 'express';
+import { Cookies } from './decorators/cookies.decorator';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -11,13 +14,20 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  singup(@Body() createUserDto: CreateUserDto) {
+  singup(@Body() createUserDto: CreateUserDto,) {
     return this.authService.resgisterUser(createUserDto)
+    
   }
 
   @Post('login')
-  login(@Body() loginUserDto: LoginUserDto) {
-    return this.authService.loginUser(loginUserDto)
+  async login(@Body() loginUserDto: LoginUserDto, @Res({passthrough: true}) response: Response, @Cookies() cookies: any) {
+    const token = await this.authService.loginUser(loginUserDto) 
+    response.cookie(TOKEN_NAME, token, {
+      httpOnly: false,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    })
   }
 
   @Patch("/:email")
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: process.env.ALLOWED_ORIGIN,
+      credentials: true,
     }
   });
 
+  app.use(cookieParser());
   const config = new DocumentBuilder()
   .setTitle('Ocso API')
   .setDescription('API for Oxxo management') 
