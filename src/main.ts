db2fb2124014ea37917d0d5738b761c205cab0d3
Emiliano Portillo <EmilianoMt>
2024-10-18 13:43:01 -0600

    Feat: Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.ALLOWED_ORIGIN,
+    }
+  });
 
   const config = new DocumentBuilder()
   .setTitle('Ocso API')
@@ -22,6 +26,6 @@ async function bootstrap() {
     transform: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
