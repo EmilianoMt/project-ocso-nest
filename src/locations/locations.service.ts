0ad9cde9a273f1585c0ce68d8cda8ccd63772b0c
Emiliano Portillo <EmilianoMt>
2024-10-25 15:04:49 -0600

    Fix: LocationId for manager

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -4,12 +4,15 @@ import { UpdateLocationDto } from './dto/update-location.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Location } from './entities/location.entity';
+import { Manager } from 'src/managers/entities/manager.entity';
 
 @Injectable()
 export class LocationsService {
   constructor(
     @InjectRepository(Location)
     private locationRepository: Repository<Location>,
+    @InjectRepository(Manager)
+    private managerRepository: Repository<Manager>
   ) {}
 
   create(createLocationDto: CreateLocationDto) {
@@ -29,13 +32,26 @@ export class LocationsService {
   }
 
   async update(id: number, updateLocationDto: UpdateLocationDto) {
+    //Set the location of the manager to null
+    this.managerRepository.createQueryBuilder().update().set({location: null}).where("locationId = :id", {
+      id,
+    }).execute();
+
     const location = await this.locationRepository.preload({
       locationId: id,
       ...updateLocationDto
     })
     if (!location) throw new NotFoundException('Location not found');
-    this.locationRepository.save(location);
-    return location
+
+    const savedLocation = await this.locationRepository.save(location);
+    
+    const updatedManager = await this.managerRepository.preload({
+      managerId: updateLocationDto.manager,
+      location: location
+    })
+    this.managerRepository.save(updatedManager);
+
+    return savedLocation
   }
 
   remove(id: number) {
